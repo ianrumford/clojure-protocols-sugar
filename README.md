c55ca973b6be4719b8d896e128087f0fa690deb0
Ian Rumford <ian@taniwha>
2014-09-01 11:32:29 +0100

    clojure-protocols-sugar-0.0.0

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# clojure-protocols-sugar
+
+Some macros and helpers for Clojure protocols and records.
+
+## Maturity
+
+"A work in progress"
+
+## Artifacts
+
+None yet.
+
+### The Most Recent Release
+
+None yet.
+
+## Documentation & Examples
+
+None yet.
+
+## Testing
+
+None yet
+
+## License
+
+Copyright (C) 2014 Ian Rumford.
+
+Licensed under [MIT](http://opensource.org/licenses/MIT)
+
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject name.rumford/clojure-protocols-sugar "0.0.0"
+  :description "Some macros and helpers for Clojure protocols and records"
+  :url "https://github.com/ianrumford/clojure-protocols-sugar"
+  :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths   ["src/test/clojure"])
